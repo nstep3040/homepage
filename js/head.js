291acb275e4791a8a9e265fcef4e81004dbcad6a
nstep3040 <ns@nstep.co.kr>
2022-05-12 18:04:25 +0900

    20220512_v1.1.3(repair)
    head, foot include 작업

---
+++ js/head.js
@@ -0,0 +1,121 @@
+document.write(`
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" user-scalable="no">
+        <meta name="naver-site-verification" content="25e242bc929ee4aa7e0ae3dc523ac84879007e5b" />
+        <meta name="robots" content="All" />
+        <meta name="Keywords" content="(주)넥스텝, 넥스텝, NextStep" />
+        <meta http-equiv="Title" content="(주)넥스텝" />
+        <meta http-equiv="Email" content="ns@nstep.co.kr" />
+        <meta http-equiv="Copyright" content="(주)넥스텝" />
+        <meta name="description" content="수중장비&middot;마그네틱&middot;RFID 및 IT DEVICE, SOFTWARE 전문기업">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="(주)넥스텝">
+        <meta property="og:description" content="수중장비&middot;마그네틱&middot;RFID 및 IT DEVICE, SOFTWARE 전문기업">
+        <!-- <meta property="og:image" content="http://www.mysite.com/myimage.jpg"> -->
+        <meta property="og:url" content="http://www.nstep.co.kr">
+        <link rel="stylesheet" href="./css/common.css?ver=1.1">
+        <title>Welcom to NextStep</title>
+    </head>
+
+    <body>
+        <div class="wrap">
+            <!-- Header -->
+            <header class="header clearfix stop-dragging">
+                <div class="top">
+                    <div class="logo">
+                        <a href="./index.html"></a>
+                    </div>
+                    <div class="topMenu">
+                        <ul class="mainMenu">
+                            <li>
+                                <a href="./html/sub_01_Company_01.html">COMPANY</a>
+                                <ul class="subMenu">
+                                    <li><a href="./html/sub_01_Company_01.html">회사소개</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="./html/sub_02_Industry_01.html">INDUSTRY</a>
+                                <ul class="subMenu">
+                                    <li><a href="./html/sub_02_Industry_01.html">수중커넥터</a></li>
+                                    <li><a href="./html/sub_02_Industry_02.html">마그네틱</a></li>
+                                    <li><a href="./html/sub_02_Industry_03.html">RFID</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="./html/sub_03_IT_01.html">IT &middot; SMART</a>
+                                <ul class="subMenu">
+                                    <li><a href="./html/sub_03_IT_01.html">PC</a></li>
+                                    <li><a href="./html/sub_03_IT_02.html">IT Device</a></li>
+                                    <li><a href="./html/sub_03_IT_03.html">Smart Solution</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="./html/sub_04_Software_01.html">SOFTWARE</a>
+                                <ul class="subMenu">
+                                    <li><a href="./html/sub_04_Software_01.html">사무용</a></li>
+                                    <li><a href="./html/sub_04_Software_02.html">연구용</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="./html/sub_05_Download_01.html">DOWNLOAD</a>
+                            </li>
+                            <li>
+                                <a href="./html/sub_06_Contact_01.html">CONTACT</a>
+                                <ul class="subMenu">
+                                    <li><a href="./html/sub_06_Contact_01.html">오시는 길</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="linked">
+                        <a href="https://blog.naver.com/nstep3040" target="_blank"></a>
+                    </div>
+                    <img id="menuMobileBtn" src="./img/menu.png">
+                </div>
+            </header>
+
+            <div class="mainMenu-mobile">
+                <details>
+                    <summary>COMPANY</summary>
+                    <ul>
+                        <li><a href="./html/sub_01_Company_01.html">회사소개</a></li>
+                    </ul>
+                </details>
+                <details>
+                    <summary>산업용</summary>
+                    <ul>
+                        <li><a href="./html/sub_02_Industry_01.html">수중커넥터</a></li>
+                        <li><a href="./html/sub_02_Industry_02.html">마그네틱</a></li>
+                        <li><a href="./html/sub_02_Industry_03.html">RFID</a></li>
+                    </ul>
+                </details>
+                <details>
+                    <summary>IT &middot; SMART</summary>
+                    <ul>
+                        <li><a href="./html/sub_03_IT_01.html">PC</a></li>
+                        <li><a href="./html/sub_03_IT_02.html">IT Device</a></li>
+                        <li><a href="./html/sub_03_IT_03.html">Smart Solution</a></li>
+                    </ul>
+                </details>
+                <details>
+                    <summary>SOFTWARE</summary>
+                    <ul>
+                        <li><a href="./html/sub_04_Software_01.html">사무용</a></li>
+                        <li><a href="./html/sub_04_Software_02.html">연구용</a></li>
+                    </ul>
+                </details>
+                <details>
+                    <summary>DOWNLOAD</summary>
+                    <ul>
+                        <li><a href="./html/sub_05_Download_01.html">자료실</a></li>
+                    </ul>
+                </details>
+                <details>
+                    <summary>CONTACT</summary>
+                    <ul>
+                        <li><a href="./html/sub_06_Contact_01.html">오시는 길</a></li>
+                    </ul>
+                </details>
+            </div>
+`);
