106c1b4446fbafce9308cdede4ab7c5e4aabbdb1
nstep3040 <ns@nstep.co.kr>
2022-05-27 17:52:15 +0900

    20220527_v1.2
    rnb, lnb 추가

---
+++ js/rnb.js
@@ -0,0 +1,12 @@
+document.write(`
+    <div class="rnb">
+        <ul class="rnb__menu">
+            <li><a href="./sub_02_Industry_03.html"><img src="../img/aegid.svg"></li></a>
+            <li><a href="./sub_02_Industry_02.html"><img src="../img/goudsmit.svg"></li></a>
+            <li><a href="#"><img src="../img/safeplast.svg"></li></a>
+            <li><a href="./sub_02_Industry_01.html"><img src="../img/teledyne_impulse.svg"></li></a>
+            <li><a href="#">블로그 바로가기</li></a>
+            <!-- <li><img src="../img/teledyne_impulse.svg"></li> -->
+        </ul>
+    </div>
+`);
